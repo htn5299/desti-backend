6c142d63b14df6c6d6c4a528463e408b4e8779a3
htn <htn5299@gmail.com>
2023-06-05 23:40:11 +0700

    json docker

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe, Logger } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({ origin: ['http://localhost:3000'], credentials: true })
+  // app.enableCors({ origin: ['http://localhost:3000'], credentials: true })
   app.setGlobalPrefix('api')
   const configService = app.get(ConfigService)
   const port = configService.get<number>('PORT')
